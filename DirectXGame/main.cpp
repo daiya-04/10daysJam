042842845ea2b8d3a23e75e785fad3e2c8eb45a7
haruki <haruki@MSI>
2023-09-13 14:45:56 +0900

    タイトルバー変更

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"ブレイクラッシュ");
+	win->CreateGameWindow(L"2001_ブレイクラッシュ");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
